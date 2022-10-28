825de18afe8fcb5b0a5590489cc560f2c834efd6
Juliana Jesus <113396337+JulianaJesus93@users.noreply.github.com>
2022-10-28 09:56:07 -0300

    Atualização de script

---
+++ README.md
@@ -0,0 +1,26 @@
+# **Criptografador**
+
+Projeto desenvolvido como atividade individual do módulo de Front-end do curso de WebDev Full-Stack do programa "Programadores Cariocas". Uma parceria entre a Prefeitura do Rio de Janeiro com o Senac Rio, tendo como metodologia de ensino a Resília Educação.
+
+---
+
+## **Status do projeto**
+
+Em desenvolvimento. (out/2022)
+
+## **Detalhamento do projeto**
+
+O desafio foi criar um site onde podemos codificar e decodificar uma mensagem utilizando dois métodos: a *Base 64* ou a *Cifra de César*.
+
+Você pode acessar meu cripfografador <a href="https://julianajesus93.github.io/Criptografia/">aqui</a>.
+
+### **Tecnologias utilizadas**
+- Javascript
+- HTML
+- CSS
+
+---
+
+## **Desenvolvedor**
+*Juliana Jesus*<br>
+Rio de Janeiro/RJ
--- javascript/script.js
+++ javascript/script.js
@@ -1,42 +1,59 @@
-//Interações com botões
+//Interação com botões codificar e decodificar
 var botao = document.querySelector('#codDecod')
 var btnB64 = document.querySelector('#envioB64')
 var btnCifra = document.querySelector('#envioCifra')
+var opcaoDecodificacao = document.querySelector('#opcaoDecodificacao')
 
-//mudar texto do botão de envio de ambos os métodos
+//muda texto do button e display do select
 botao.addEventListener('click', function () {
     if (decodificar.checked) {
         btnB64.innerText = 'Decodificar em Base 64';
         btnCifra.innerText = 'Decodificar em Cifra de César';
+        opcaoDecodificacao.style.display = 'block';
     } else  if (codificar.checked) {
         btnB64.innerText = 'Codificar em Base 64';
         btnCifra.innerText = 'Codificar em Cifra de César';
+        opcaoDecodificacao.style.display = 'none';
+        metodoCifra.style.display = 'block';
+        metodoB64.style.display = 'block';
     }
 })
 
+//Interação com select de método de decodificação
+var select = document.querySelector("#opcao")
+var metodos = document.querySelector("#metodos")
+var metodoB64 = document.querySelector(".metodoB64")
+var metodoCifra = document.querySelector(".metodoCifra")
+
+opcaoDecodificacao.addEventListener('click', function () {
+    if (select.value === "base64") {
+        metodoB64.style.display = 'block';
+        metodoCifra.style.display = 'none';
+    } else if (select.value === "cifraDeCesar") {
+        metodoCifra.style.display = 'block';
+        metodoB64.style.display = 'none';
+    }
+})
 
 
 //Métodos de codificações e decodificações
 var codificar = document.querySelector('#codificar')
 var decodificar = document.querySelector('#decodificar')
 
-
 //BASE 64
 function base64() {
     var mensagem = document.getElementById("mensagem").value
-  
+
     if (codificar.checked) {
         var codificado = btoa(mensagem)
         document.getElementById("respostaB64").value = codificado
     } else if (decodificar.checked) {
         var decodificado = atob(mensagem)
         document.getElementById("respostaB64").value = decodificado
-    } 
+    }
 }
 
-
 //CIFRA DE CESAR
-
 //opções de incremento
 const incrementoOpcoes = document.querySelector("#incremento")
 var opcoes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
@@ -59,10 +76,10 @@ function cifraDeCesar() {
                 if (mensagem[i] == caracteres[j]) {
                     mensagemFinal += caracteres[(j + incremento) % caracteres.length]
                 }
-            }    
+            }
         }
         document.getElementById("respostaCifra").value = mensagemFinal
-
+            
     } else if (decodificar.checked) {
         for(var i = 0; i < mensagem.length; i++) {
             for(var j = caracteres.length - 1; j >= 0; j--) {
@@ -71,8 +88,10 @@ function cifraDeCesar() {
                 } else if (mensagem[i] == caracteres[j] && j - incremento < 0) {
                     mensagemFinal += caracteres[((j - incremento) % caracteres.length) + caracteres.length]
                 }
-            }    
+            }
         }
         document.getElementById("respostaCifra").value = mensagemFinal
     }
 }
+
+//cifra não está caractere sensível, e está ignorando espaços ou caracteres especiais.
